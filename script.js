50acb1d4f45c2bdee520a0eb2f183f09a3e759c7
shubhlc194 <shubhamkc194@gmail.com>
2025-01-09 02:57:28 +0530

    add js

--- script.js
+++ script.js
@@ -1,56 +1,84 @@
-const inputSlider=document.querySelector("[ data-lengthSlider]")
+// Handle Slider Control and Display Password Length 
+let lengthDisplay = document.querySelector('[lengthDisplay');
+// console.log(lengthDisplay)
+let slider = document.querySelector('input[type=range]');
+// console.log(slider)
+
+
+function handleSlider() {
+    slider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+}
+
+let passwordLength = 10;
+handleSlider();
+
+slider.addEventListener('input', (event) => {
+    passwordLength = event.target.value;
+    handleSlider();
+});
+
+// --------------------------------------
+
+// Generate Random Letters and Number and Symbols
+const symbol = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+function generateRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Random Lowercase Letter 
+function generateRandomLowercase() {
+    return String.fromCharCode(generateRandom(97, 123));
+}
+
+// Random Lowercase Letter 
+function generateRandomUppercase() {
+    return String.fromCharCode(generateRandom(65, 91));
+}
+
+// Random Number 
+function generateRandomNumber() {
+    return generateRandom(1, 10);
+}
+
+// Generate Symbol 
+function generateRandomSymbol() {
+    let index = generateRandom(0, symbol.length);
+    return symbol[index];
+}
+
+// console.log(generateRandomLowercase());
+// console.log(generateRandomUppercase());
+// console.log(generateRandomNumber());
+// console.log(generateRandomSymbol());
+
+// --------------------------------------
+
+
+// Strength Color Based on Password 
+let indicator = document.querySelector('.indicator');
+
+// Set Indicator 
+function setIndicator(color) {
+    indicator.style.backgroundColor = color;
+    indicator.style.boxShadow = `0 0 12px 1px ${color}`;
+}
+
+// Default Indicator 
+setIndicator("#ccc");
+
+const uppercase = document.querySelector('#uppercase');
 const lowercase = document.querySelector('#lowercase');
 const numbers = document.querySelector('#numbers');
 const symbols = document.querySelector('#symbols');
-let lengthDisplay = document.querySelector('[lengthDisplay');
-let generateBtn = document.querySelector("#generateBtn");
-// console.log(lengthDisplay)
-let slider = document.querySelector('input[type=range]');
-let checkBoxes = document.querySelectorAll("input[type=checkbox]");
-let indicator = document.querySelector('.indicator');
-let copyMessage = document.querySelector("[copyMessage]");
-let copyBtn = document.querySelector(".copyBtn");
-let passwordDisplay = document.querySelector("input[passwordDisplay]");
-const Symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-let password="";
-let passwordLength=15;
-let checkcount=1;
-handleSlider();
-// set strength circle color to grey
-
-
-// set passwordlength
-function handleSlider(){
-    inputSlider.value=passwordLength;
-    lengthDisplay.innerText=passwordLength;
-    //
-
-}
-function setIndicator(color){
-     indicator.style.backgroundColor=color;
-}
-function getRandomInteger(min,max){
-    Math.floor(Math.random()*(max-min))+min;
-}
-function genrateRandomNumber(){
-    return getRandomInteger(0,9);
-}
-function lowerCase(){
-    return String.fromCharCode(getRandomInteger(97,123));
-}
-function upperCase(){
-    return String.fromCharCode(getRandomInteger(65,91));
-}
-function genrateSymbol(){
-  const randNum=getrndInteger(0,symbols.length);
-  return symbols.charAt(randNUm);
-}
 
 function calcStrength() {
     let hasUpper = false;
     let hasLower = false;
     let hasNumber = false;
     let hasSymbol = false;
+
     if (uppercase.checked) hasUpper = true;
     if (lowercase.checked) hasLower = true;
     if (numbers.checked) hasNumber = true;
@@ -69,6 +97,15 @@ function calcStrength() {
     }
 }
 
+// -----------------------------------
+
+// Copy Message 
+let copyMessage = document.querySelector("[copyMessage]");
+let copyBtn = document.querySelector(".copyBtn");
+let passwordDisplay = document.querySelector("input[passwordDisplay]");
+// passwordDisplay.value = "My Name is Priyansh";
+
+// Why we use it - https://stackoverflow.com/questions/45071353/copy-text-string-on-click#:~:text=15-,Use%20the%20Clipboard,-API!
 async function copyContent() {
     try {
         await navigator.clipboard.writeText(passwordDisplay.value);
@@ -86,3 +123,96 @@ async function copyContent() {
         copyMessage.classList.remove('active');
     }, 2000)
 }
+
+copyBtn.addEventListener("click", () => {
+    if (passwordDisplay.value)
+        copyContent();
+});
+
+// ------------------------------------ 
+
+// shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+// Shuffle the array randomly - Fisher Yates Method
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    let str = "";
+    array.forEach((el) => (str += el));
+    return str;
+}
+// Password Generate 
+
+// By Default UpperCase Checked 
+// uppercase.checked = true;
+
+let checkBoxes = document.querySelectorAll("input[type=checkbox]");
+// console.log(checkBoxes);
+
+let checkCount = 0;
+
+// CheckBox - Handle 
+
+function handleCheckBoxChange() {
+    checkCount = 0;
+    checkBoxes.forEach((checkbox) => {
+        if (checkbox.checked)
+            checkCount++;
+    });
+
+    //special condition
+    if (passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+}
+
+checkBoxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', handleCheckBoxChange);
+})
+
+
+let password = "";
+let generateBtn = document.querySelector("#generateBtn");
+
+generateBtn.addEventListener('click', () => {
+    if (checkCount <= 0)
+        return;
+
+    if (passwordLength < checkCount) {
+        passwordLength = checkCount;
+        handleSlider();
+    }
+
+    // Remove Previous Password 
+    password = "";
+
+    let arrayOfCheckedFunction = [];
+
+    if (uppercase.checked) arrayOfCheckedFunction.push(generateRandomUppercase);
+    if (lowercase.checked) arrayOfCheckedFunction.push(generateRandomLowercase);
+    if (numbers.checked) arrayOfCheckedFunction.push(generateRandomNumber);
+    if (symbols.checked) arrayOfCheckedFunction.push(generateRandomSymbol);
+
+    // Compulsory Addition
+    for (let i = 0; i < arrayOfCheckedFunction.length; i++) {
+        password += arrayOfCheckedFunction[i]();
+    }
+
+    // console.log("Password: " + password);
+
+    // Additional addition
+    for (let i = 0; i < passwordLength - arrayOfCheckedFunction.length; i++) {
+        let randIndex = generateRandom(0, arrayOfCheckedFunction.length);
+        password += arrayOfCheckedFunction[randIndex]();
+    }
+    // console.log("Password: " + password);
+
+    // Shuffle Password 
+    password = shuffle(Array.from(password));
+    passwordDisplay.value = password;
+    calcStrength();
+});
